bdf000fb41e60fd71c90aef7d7b581117f2a17e3
Jose Tejada <jotego@gmail.com>
2020-01-04 07:55:20 +0100

    TCL

---
+++ hdl/jtdd.tcl
@@ -0,0 +1,4 @@
+set_global_assignment -name VERILOG_MACRO "CORENAME=\"JTDD\""
+set_global_assignment -name VERILOG_MACRO "GAMETOP=jtdd_game"
+set_global_assignment -name VERILOG_MACRO "COLORW=4"
+# set_global_assignment -name VERILOG_MACRO "AVATARS=1"
