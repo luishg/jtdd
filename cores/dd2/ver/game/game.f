55c50ef5e9a880a70c26d1ec405ecdc15bda0857
Jose Tejada <jotego@gmail.com>
2020-01-04 12:36:45 +0100

    sim

---
+++ cores/dd2/ver/game/game.f
@@ -0,0 +1,31 @@
+../../hdl/jtdd2_game.v
+../../hdl/jtdd2_sub.v
+../../../dd/hdl/jtdd_main.v
+../../../dd/hdl/jtdd_dip.v
+../../../dd/hdl/jtdd_video.v
+../../../dd/hdl/jtdd_timing.v
+../../../dd/hdl/jtdd_char.v
+../../../dd/hdl/jtdd_scroll.v
+../../../dd/hdl/jtdd_obj.v
+../../../dd/hdl/jtdd_colmix.v
+../../../dd/hdl/jtdd_prom_we.v
+# ../../../dd/hdl/jtdd_sound.v
+
+../../../modules/jtframe/hdl/ram/jtframe_prom.v
+../../../modules/jtframe/hdl/ram/jtframe_ram.v
+../../../modules/jtframe/hdl/ram/jtframe_dual_ram.v
+# clocking
+../../../modules/jtframe/hdl/clocking/jtframe_cen48.v
+../../../modules/jtframe/hdl/jtframe_z80wait.v
+# SDRAM
+../../../modules/jtframe/hdl/sdram/jtframe_rom.v
+../../../modules/jtframe/hdl/sdram/jtframe_romrq.v
+
+# CPU
+../../../modules/jtframe/hdl/cpu/jtframe_sys6809.v
+../../../modules/jtframe/hdl/cpu/jtframe_z80.v
+# Other
+../../../modules/jtframe/hdl/jtframe_ff.v
+../../../modules/jtframe/hdl/jtframe_sh.v
+../../../modules/jtframe/hdl/sound/jtframe_mixer.v
+../../../modules/jtframe/hdl/video/jtframe_charmsg.v
